0e5cfe57f958139ab5ea2653b3b68c80c9630747
bonf <dan.bonifazi@gmail.com>
2026-01-13 10:39:41 +0100

    added assert_difference with deltas and multiple schemas

--- lib/bonf/custom_assertions.ex
+++ lib/bonf/custom_assertions.ex
@@ -1,4 +1,41 @@
 defmodule Bonf.CustomAssertions do
+  defmacro assert_difference(schema_deltas, run_fn) do
+    quote do
+      before =
+        unquote(schema_deltas)
+        |> Map.keys()
+        |> Enum.map(&{&1, Repo.count(&1)})
+        |> Map.new()
+
+      unquote(run_fn).()
+
+      after_ =
+        unquote(schema_deltas)
+        |> Map.keys()
+        |> Enum.map(&{&1, Repo.count(&1)})
+        |> Map.new()
+
+      Enum.each(before, fn {schema, value1} ->
+        delta = Map.get(unquote(schema_deltas), schema, 0)
+        value2 = Map.get(after_, schema, 0)
+
+        assert value2 == value1 + delta,
+               "expected #{inspect(schema)} count to change by #{delta}, but changed by #{value2 - value1}"
+      end)
+    end
+  end
+
+  defmacro assert_no_difference(schemas, run_fn) when is_list(schemas) do
+    quote do
+      schema_deltas =
+        unquote(schemas)
+        |> Enum.map(&{&1, 0})
+        |> Map.new()
+
+      assert_difference(schema_deltas, unquote(run_fn))
+    end
+  end
+
   defmacro assert_difference(count_fn, delta, run_fn) do
     quote do
       value1 = unquote(count_fn)
